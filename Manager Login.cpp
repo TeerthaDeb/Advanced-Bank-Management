7f51560f560c486721bc02b24c14e01d9fe12aae
TeerthaDeb <teertha.deb579@gmail.com>
2022-03-08 00:35:29 -0600

    Update Manager Login.cpp

--- Manager Login.cpp
+++ Manager Login.cpp
@@ -71,8 +71,9 @@ class customers
 {
 	private:
 		static long long int total_customers;
-private:
+	private:
 	//variables:
+		bool active_status = false;
 		char name[50] , address[20], phone_number[50] , sex , password[50] , city[50] , province[50] , country[50];
 		double checking_amount = 0 , saving_amount = 0 , visa_amount = 0 , interest_rate , points = 0 , loans = 0;
 		long long int account_number = 100000000 + (rand() % 899999999) , visa_number = 1000000000 + (rand() % 8999999999) , account_creators_id = 0 , employee_number = 0 , last_modified_by = 0;
@@ -80,188 +81,188 @@ private:
 		short int birth_year , birth_month , birth_date , account_create_date , account_create_month , account_create_year , made_transection = 0;
 		transection transfers[50];
 		//functions:
-public:
-	//variables:
+	public:
+		//variables:
 
-	//functions:
+		//functions:
 
-	//default constructor:
-	customers()
-	{
-	}
+		//default constructor:
+			customers()
+			{
+			}
 
-	//constructor(s):
-	customers(string user_name , string user_password , string user_address , string user_city , string user_province , string user_country , long long int account_creator_id , short int user_birth_date , short int user_birth_month , short int user_birth_year , char user_sex , string user_phone_number , double user_checking_amount = 0 , double user_saving_amount= 0 , double user_interest_rate = default_interest_rate)
-	{
-		total_customers++;
-		set_every_thing_for_user(user_name , user_password , user_address , user_city , user_province , user_country , account_creator_id , user_birth_date , user_birth_month , user_birth_year , user_sex , user_phone_number ,  user_checking_amount , user_saving_amount , user_interest_rate);
-	}
+		//constructor(s):
+			customers(string user_name , string user_password , string user_address , string user_city , string user_province , string user_country , long long int account_creator_id , short int user_birth_date , short int user_birth_month , short int user_birth_year , char user_sex , string user_phone_number , double user_checking_amount = 0 , double user_saving_amount= 0 , double user_interest_rate = default_interest_rate)
+			{
+				total_customers++;
+				set_every_thing_for_user(user_name , user_password , user_address , user_city , user_province , user_country , account_creator_id , user_birth_date , user_birth_month , user_birth_year , user_sex , user_phone_number ,  user_checking_amount , user_saving_amount , user_interest_rate);
+			}
 
-	customers(long long int account_creator)
-	{
-		total_customers++;
-        account_creators_id = account_creator;
-	}
+			customers(long long int account_creator)
+			{
+				total_customers++;
+        		account_creators_id = account_creator;
+			}
 
-	//copy constructors:
-	/*customers(const customers &object)
-	{
+			//copy constructors:
+			/*customers(const customers &object)
+			{
 
-		return *this;
-	}*/
+				return *this;
+			}*/
 
-	//destructor:
-	~customers()
-	{
+			//destructor:
+			~customers()
+			{
 
-	}
-	//setters:
-	void set_user_password(string new_password)
-	{
-		strcpy(password , new_password.c_str());
-	}
-	void set_user_name(string user_name)
-	{
-		strcpy(name , user_name.c_str());
-	}
-	void set_user_address(string user_address , string user_city , string user_province , string user_country)
-	{
-		strcpy(address , user_address.c_str());
-		strcpy(city , user_city.c_str());
-		strcpy(province , user_province.c_str());
-		strcpy(country , user_country.c_str());
-	}
-	void set_user_checking_amount(double user_checking_amount)
-	{
-		checking_amount = user_checking_amount;
-	}
-	void set_user_saving_amount(double user_saving_amount)
-	{
-		saving_amount = user_saving_amount;
-	}
-	void set_user_interest_rate(double user_interest_rate = default_interest_rate)
-	{
-		interest_rate = user_interest_rate / 100;
-	}
-	void set_user_visa_amount(double user_visa_amount)
-	{
-		if(have_visa)
-		{
-			visa_amount = user_visa_amount;
-		}
-		else
-		{
-			printf("\aSorry. The user don't have any visa card right now.");
-		}
-	}
-	void set_user_birth_date(short int user_birth_date , short int user_birth_month , short int user_birth_year)
-	{
-		birth_date = user_birth_date;
-		birth_month = user_birth_month;
-		birth_year = user_birth_year;
-	}
-	void set_every_thing_for_user(string user_name , string user_password , string user_address , string user_city , string user_province , string user_country , long long int account_creator_id , short int user_birth_date , short int user_birth_month , short int user_birth_year , char user_sex , string user_phone_number , double user_checking_amount = 0 , double user_saving_amount= 0 , double user_interest_rate = default_interest_rate)
-	{
-		set_user_name(user_name);
-		set_user_address(user_address , user_city , user_province , user_country);
-		set_user_password(user_password);
-		set_user_checking_amount(user_checking_amount);
-		set_user_saving_amount(user_saving_amount);
-		visa_amount = 0;
-		set_user_interest_rate(user_interest_rate);
-		account_creators_id = account_creator_id;
-		set_user_birth_date(user_birth_date , user_birth_month , user_birth_year);
-		sex = user_sex;
-		set_users_phone_number(user_phone_number);
-	}
-	void set_users_phone_number(string user_phone_number)
-	{
-		strcpy(phone_number , user_phone_number.c_str());
-	}
-	//getters:
-	void print_everything()
-	{
-		cout<<"name: "<<name<<" ;acount_number: "<<account_number<<" ;total: "<<saving_amount + checking_amount + visa_amount<<" ;interest_rate: "<<interest_rate<<" ;Loans: "<<loans<<" ;last modified by: "<<last_modified_by <<'\n';
-	}
-	long long int get_total_customers()
-	{
-		return total_customers;
-	}
-	long long int get_account_number()
-	{
-		return account_number;
-	}
-	long long int get_visa_number()
-	{
-		if(have_visa){
-			return visa_number;
-		}
-		else{
-			return -1;
-		}
-	}
-	long long int get_account_creators_id()
-	{
-		return account_creators_id;
-	}
-	string get_name()
-	{
-		return name;
-	}
-	//others:
-	void request_for_visa(long long int last_modifier_id)
-	{
-		have_visa = true;
-		last_modified_by = last_modifier_id;
-	}
-	void create_new_account_number()
-	{
-		account_number = 100000000 + (rand() % 899999999) + 3;
-	}
-	void modify_account(long long int modifiers_id , string user_name , string user_address , string user_city , string user_province , string user_country , double user_interest_rate , short int user_birth_date , short int user_birth_month , short int user_birth_year , char user_sex , string user_phone_number ,  bool user_is_employee_of_the_bank , long long int user_employee_number)
-	{
-		set_every_thing_for_user(user_name , password , user_address , user_city , user_province , user_country , account_creators_id , user_birth_date , user_birth_month , user_birth_year , user_sex , user_phone_number , checking_amount , saving_amount , interest_rate);
-		last_modified_by = modifiers_id;
-	}
-	void print_every_details_for_customer()
-	{
-		gxy(5 , 1) , cout<<"account number:			"<<account_number;
-		gxy(5 , 2) , cout<<"Name:				"<<name;
-		gxy(5 , 3) , cout<<"Address:				"<<address<<", "<<city<<", "<<province<<", "<<country;
-		gxy(5 , 4) , cout<<"Phone Number:			"<<phone_number;
-		gxy(5 , 5) , cout<<"Sex:				"<<sex;
-		gxy(5 , 6) , cout<<"Birh Date:				"<<birth_date<<"\\"<<birth_month<<"\\"<<birth_year;
-		gxy(5 , 7) , cout<<"Checking Amount:			"<<checking_amount;
-		gxy(5 , 8) , cout<<"Saving Amount:			"<<saving_amount;
-		if(have_visa){
-			gxy(5 , 9) , cout<<"Visa Amount:	"<<visa_amount;
-		}
-		else{
-			gxy(5 , 9) , cout<<"No Visa for this account";
-		}
-		gxy(5 , 10) , cout<<"interest rate:			"<<interest_rate;
-		gxy(5 , 11) , cout<<"account created by:		"<<account_creators_id;
-		if(is_employee){
-			gxy(5 , 12) , cout<<"Employee number:	"<<employee_number;
-		}
-		else{
-			gxy(5 , 12) , cout<<"Not an bank employee";
-		}
-		gxy(5 , 13) , cout<<"Last modified by:			"<<last_modified_by;
-		gxy(5 , 14) , cout<<"Made_transections:			"<<made_transection;
-	}
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////new
-	//getters
-	//friend functions:
-	//deposits
-	void deposit_into_checking(double amount)
-	{
-		checking_amount += amount;
-	}
-	void deposit_into_saving(double amount)
-	{
-		saving_amount += amount;
-	}
+			}
+		//setters:
+			void set_user_password(string new_password)
+			{
+				strcpy(password , new_password.c_str());
+			}
+			void set_user_name(string user_name)
+			{
+				strcpy(name , user_name.c_str());
+			}
+			void set_user_address(string user_address , string user_city , string user_province , string user_country)
+			{
+				strcpy(address , user_address.c_str());
+				strcpy(city , user_city.c_str());
+				strcpy(province , user_province.c_str());
+				strcpy(country , user_country.c_str());
+			}
+			void set_user_checking_amount(double user_checking_amount)
+			{
+				checking_amount = user_checking_amount;
+			}
+			void set_user_saving_amount(double user_saving_amount)
+			{
+			saving_amount = user_saving_amount;
+			}
+			void set_user_interest_rate(double user_interest_rate = default_interest_rate)
+			{
+				interest_rate = user_interest_rate / 100;
+			}
+			void set_user_visa_amount(double user_visa_amount)
+			{
+				if(have_visa)
+				{
+					visa_amount = user_visa_amount;
+				}
+				else
+				{
+					printf("\aSorry. The user don't have any visa card right now.");
+				}
+			}
+			void set_user_birth_date(short int user_birth_date , short int user_birth_month , short int user_birth_year)
+			{
+				birth_date = user_birth_date;
+				birth_month = user_birth_month;
+				birth_year = user_birth_year;
+			}
+			void set_every_thing_for_user(string user_name , string user_password , string user_address , string user_city , string user_province , string user_country , long long int account_creator_id , short int user_birth_date , short int user_birth_month , short int user_birth_year , char user_sex , string user_phone_number , double user_checking_amount = 0 , double user_saving_amount= 0 , double user_interest_rate = default_interest_rate)
+			{
+				set_user_name(user_name);
+				set_user_address(user_address , user_city , user_province , user_country);
+				set_user_password(user_password);
+				set_user_checking_amount(user_checking_amount);
+				set_user_saving_amount(user_saving_amount);
+				visa_amount = 0;
+				set_user_interest_rate(user_interest_rate);
+				account_creators_id = account_creator_id;
+				set_user_birth_date(user_birth_date , user_birth_month , user_birth_year);
+				sex = user_sex;
+				set_users_phone_number(user_phone_number);
+			}
+			void set_users_phone_number(string user_phone_number)
+			{
+				strcpy(phone_number , user_phone_number.c_str());
+			}
+			//getters:
+			void print_everything()
+			{
+				cout<<"name: "<<name<<" ;acount_number: "<<account_number<<" ;total: "<<saving_amount + checking_amount + visa_amount<<" ;interest_rate: "<<interest_rate<<" ;Loans: "<<loans<<" ;last modified by: "<<last_modified_by <<'\n';
+			}
+			long long int get_total_customers()
+			{
+				return total_customers;
+			}
+			long long int get_account_number()
+			{
+				return account_number;
+			}
+			long long int get_visa_number()
+			{
+				if(have_visa){
+					return visa_number;
+				}
+				else{
+					return -1;
+				}
+			}
+			long long int get_account_creators_id()
+			{
+				return account_creators_id;
+			}
+			string get_name()
+			{
+				return name;
+			}
+		//others:
+			void request_for_visa(long long int last_modifier_id)
+			{
+				have_visa = true;
+				last_modified_by = last_modifier_id;
+			}
+			void create_new_account_number()
+			{
+				account_number = 100000000 + (rand() % 899999999) + 3;
+			}
+			void modify_account(long long int modifiers_id , string user_name , string user_address , string user_city , string user_province , string user_country , double user_interest_rate , short int user_birth_date , short int user_birth_month , short int user_birth_year , char user_sex , string user_phone_number ,  bool user_is_employee_of_the_bank , long long int user_employee_number)
+			{
+				set_every_thing_for_user(user_name , password , user_address , user_city , user_province , user_country , account_creators_id , user_birth_date , user_birth_month , user_birth_year , user_sex , user_phone_number , checking_amount , saving_amount , interest_rate);
+				last_modified_by = modifiers_id;
+			}
+			void print_every_details_for_customer()
+			{
+				gxy(5 , 1) , cout<<"account number:			"<<account_number;
+				gxy(5 , 2) , cout<<"Name:				"<<name;
+				gxy(5 , 3) , cout<<"Address:				"<<address<<", "<<city<<", "<<province<<", "<<country;
+				gxy(5 , 4) , cout<<"Phone Number:			"<<phone_number;
+				gxy(5 , 5) , cout<<"Sex:				"<<sex;
+				gxy(5 , 6) , cout<<"Birh Date:				"<<birth_date<<"\\"<<birth_month<<"\\"<<birth_year;
+				gxy(5 , 7) , cout<<"Checking Amount:			"<<checking_amount;
+				gxy(5 , 8) , cout<<"Saving Amount:			"<<saving_amount;
+				if(have_visa){
+					gxy(5 , 9) , cout<<"Visa Amount:	"<<visa_amount;
+				}
+				else{
+					gxy(5 , 9) , cout<<"No Visa for this account";
+				}
+				gxy(5 , 10) , cout<<"interest rate:			"<<interest_rate;
+				gxy(5 , 11) , cout<<"account created by:		"<<account_creators_id;
+				if(is_employee){
+					gxy(5 , 12) , cout<<"Employee number:	"<<employee_number;
+				}
+				else{
+					gxy(5 , 12) , cout<<"Not an bank employee";
+				}
+				gxy(5 , 13) , cout<<"Last modified by:			"<<last_modified_by;
+				gxy(5 , 14) , cout<<"Made_transections:			"<<made_transection;
+			}
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////new
+		//getters
+		//friend functions:
+		//deposits
+			void deposit_into_checking(double amount)
+			{
+				checking_amount += amount;
+			}
+			void deposit_into_saving(double amount)
+			{
+				saving_amount += amount;
+			}
 };
 
 long long int customers::total_customers = 0;
@@ -1004,23 +1005,30 @@ void main_menu(bank_employee current_employee)
 
 void show_full_details_of_an_employee()
 {
-	FILE *employee_file_pointer;
-	employee_file_pointer = fopen("EMPLOYERS_INDEX.TXT" , "r");
+	fstream file_pointer;
+	file_pointer.open("EMPLOYERS_INDEX.TXT" , ios::in);
+	//FILE *employee_file_pointer;
+	//employee_file_pointer = fopen("EMPLOYERS_INDEX.TXT" , "r");
 	bank_employee employee;
-	long long int employee_number;
+	long long int employee_number , index_number;
 	bool found_employee = false;
 	system("cls");
 	drawboard();
 	gxy(6 , 4),printf("Enter the account number to show full info of the account: ");
 	cin>>employee_number;
 	gxy(6 , 7) , printf("Searching");
-	while(fread(&employee, sizeof(employee), 1, employee_file_pointer)){
-        if(employee_number == employee.get_employers_number()){
+	while(!file_pointer.eof()){
+		file_pointer >> index_number;
+        if(employee_number == index_number){
 			found_employee = true;
+			fstream database_file_pointer;
+			database_file_pointer.open("Employers_Record\\" + to_string(employee_number) + ".txt" , ios::in);
+			database_file_pointer.read((char*) &employee , sizeof(employee));
+			database_file_pointer.close();
 			system("cls");
 			drawboard();
-           	employee.show_full_details();
-			break;
+			employee.show_full_details();
+           	break;
     	}
 		else if(found_employee == false){
 			printf(". ");
@@ -1029,7 +1037,7 @@ void show_full_details_of_an_employee()
 	if(found_employee == false){
 		gxy(6 , 10) , printf("No account found with this number");
 	}
-	fclose(employee_file_pointer);
+	file_pointer.close();
 	gxy(5 , 20);
 	printf("Press any key to go to main menu again...\b");
 	getch();
@@ -1045,23 +1053,23 @@ void main_menu_for_managers(bank_employee current_employee)
 	drawboard();
 	gxy(width / 2 - 30 , 1) , 	printf(">>>>>>>>>>>>>>>>>>>>>>	 Main Menu	<<<<<<<<<<<<<<<<<<<<<<<<<");
 	gxy(width / 2 - 30 , 3) , 	printf("###################	    New Clients	    ###################");
-	gxy(30 , 4),		printf("[0] create a client account");
-	gxy(30 , 5),		printf("[1] show accounts");
-	gxy(30 , 6),		printf("[2] modify an account");
-	gxy(30 , 7),		printf("[3] delete an account");
-	gxy(30 , 8),		printf("[4] show full details of an account");
-	gxy(30 , 9),		printf("[5] deposit money into an account");
+	gxy(30 , 4),				printf("[0] create a client account");
+	gxy(30 , 5),				printf("[1] show accounts");
+	gxy(30 , 6),				printf("[2] modify an account");
+	gxy(30 , 7),				printf("[3] delete an account");
+	gxy(30 , 8),				printf("[4] show full details of an account");
+	gxy(30 , 9),				printf("[5] deposit money into an account");
 	gxy(width / 2 - 30 , 11) , 	printf("###################	  your account  	###################");
-	gxy(30 , 13),		printf("[6] Show your created accounts");
-	gxy(30 , 14),		printf("[7] change your password");
-	gxy(30 , 15),		printf("[8] change your account details");
-	gxy(30 , 16),		printf("[9] log out");
+	gxy(30 , 13),				printf("[6] Show your created accounts");
+	gxy(30 , 14),				printf("[7] change your password");
+	gxy(30 , 15),				printf("[8] change your account details");
+	gxy(30 , 16),				printf("[9] log out");
 	gxy(width / 2 - 30 , 17) , 	printf("###################	  employers account	  ###################");
-	gxy(30 , 18),		printf("[.] Show full details of an employer");
-	gxy(30 , 19),		printf("[+] Add new employee.");
-	gxy(30 , 20),		printf("[*] show all employee");
-	gxy(30 , 21),		printf("[/] Modify Employers Account");
-	gxy(30 , 22),		printf("[-] Delete an employee");
+	gxy(30 , 18),				printf("[.] Show full details of an employer");
+	gxy(30 , 19),				printf("[+] Add new employee.");
+	gxy(30 , 20),				printf("[*] show all employee");
+	gxy(30 , 21),				printf("[/] Modify Employers Account");
+	gxy(30 , 22),				printf("[-] Delete an employee");
 	gxy(width / 2 - 10 , 24) ,	printf("Enter your selection..:  \b");
 	main_menu_choice = getch();
 	switch (main_menu_choice){
@@ -1071,32 +1079,26 @@ void main_menu_for_managers(bank_employee current_employee)
 		}
 		case '1' :{
 						read_all_customers(current_employee);
-						//Modify_customers_account();
 						break;
 		}
 		case '2' :{
 						Modify_customers_account(current_employee);
-						//read_all_customers(current_employee);
 						break;
 		}
 		case '3' :{
 						Remove_Customers_account();
-						//created_accounts_by_the_user(current_employee);
 						break;
 		}
 		case '4' :{
 						show_full_account_details();
-						//Remove_Customers_account();
 						break;
 		}
 		case '6' : {
 						created_accounts_by_the_user(current_employee);
-						//create_employee();
 						break;
 		}
 		case '7' :	{
 						change_employee_password(current_employee);
-						//read_all_employees();
 						break;
 		}
 		case '9' :	{
@@ -1105,12 +1107,10 @@ void main_menu_for_managers(bank_employee current_employee)
 		}
 		case '+' :	{
 						create_employee();
-						//change_employee_password(current_employee);
 						break;
 		}
 		case '*' :{
 						read_all_employees();
-						//log_out(current_employee);
 						break;
 		}
 		case '.' :{
@@ -1219,7 +1219,5 @@ int main()
 {
 	/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>							Should run from here                                             <<<<<<<<<<<<<<<<<<<<<<<<<<<*/
 	log_in_screen();
-	//create_employee();
-
 	return 0;
 }
